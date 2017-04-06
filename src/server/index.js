a5429f989007d004a200a0ed7297e70208debd26
Ben Milton <ben.c.milton@gmail.com>
2017-04-06 09:08:02 -0400

    more

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,8 @@
-// server/index.js
 'use strict';
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}!`);
